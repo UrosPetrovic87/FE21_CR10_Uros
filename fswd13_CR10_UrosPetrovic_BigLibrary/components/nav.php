6bf54b2902701d64eace82640d60fa73b24ba5bd
UrosPetrovic87 <petrovicuros1987@gmail.com>
2021-08-27 10:32:14 +0200

    Update nav.php

--- fswd13_CR10_UrosPetrovic_BigLibrary/components/nav.php
+++ fswd13_CR10_UrosPetrovic_BigLibrary/components/nav.php
@@ -20,25 +20,3 @@
 
 
 
-
-
-
-
-
-
-
-<!-- <nav class=" navbar  sticky-top shadow navbar-expand-lg navbar-light bg-light">
-        <div class=" container d-flex flex-row justify-content-between ">
-            <div >
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a href= "index.php"><button class='btn btn-outline-dark btn-sm border-0'type="button" >Home</button></a>
-                </li>
-            </ul>
-            </div>
-            <div class="h2 text-dark">Big Library</div>
-            <div>
-            <a href= "create.php"><button class='btn btn-outline-dark btn-sm border-0'type="button" >Add Media</button></a>
-            </div>
-        </div>
-</nav> -->
